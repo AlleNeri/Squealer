67abdd0f2c4e1641c128572148ec84928d73db76
SaadMedhat <saad2001f@gmail.com>
2023-10-13 10:02:30 +0200

    Login form

--- social/src/Login.jsx
+++ social/src/Login.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './login.css'
-export default function Login() {
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // send login request
+  }
+
   return (
-    <div id="login">
-      <p>LOGIN PAGE</p>
-    </div>
-  )
+    <form class='login-form' onSubmit={handleSubmit}>
+      <label class='login-form label'>
+        Username:
+        <input 
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)} 
+        />
+      </label>
+
+      <label class='login-form label'>
+        Password: 
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+
+      <button class='login-form button' type="submit">Login</button>
+    </form>
+  );
 }
+
+export default Login;
