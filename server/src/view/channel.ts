c62bccd061f4158364e437445e541f441900e0e8
AlleNeri <alessandro.neri21@studio.unibo.it>
2024-01-25 19:08:30 +0100

    fix(server): channel authorization

--- server/src/view/channel.ts
+++ server/src/view/channel.ts
@@ -24,20 +24,27 @@ channelRoute.get("/all", Auth.softAuthorize, (req: Request, res: Response) => {
 });
 
 //get all posts of a channel
-//the channel has to be specified in the query: http ... /channels/{channelId}/posts
-channelRoute.get("/:id/posts", (req: Request, res: Response) => {
-	const channel: string=req.params.id;
+//if the channel is private, the user has to be logged in and has to appertain to the channel
+//the channel has to be specified in the query: http[...]/channels/{channelId}/posts
+channelRoute.get("/:id/posts", Auth.softAuthorize, async (req: Request, res: Response) => {
+	const channelId: string=req.params.id;
+	const channel: Channel | null = await ChannelSchema.findById(channelId);
+
+	if(!channel) return res.status(404).json({ msg: "Channel not found" });
+	else if(channel.private && !req.user) return res.status(401).json({ msg: "You are not logged in" });
+	else if(channel.private && !req.user?.appartenence.contains(channelId)) return res.status(401).json({ msg: "You do not have access to this channel" });
+
 	PostSchema.find({
 		$or: [
-			{ posted_on: channel },
-			{ appartains_to: channel }
+			{ posted_on: channelId },
+			{ appartains_to: channelId }
 		]
 	})
 		.then((posts: Post[]) => res.status(200).json(posts))
 		.catch((err: Error) => res.status(400).json({err: err, channel: channel}));
 });
 
-//get a specific channel
+//get the info of a specific channel
 channelRoute.get("/:id", (req: Request, res: Response) => {
 	ChannelSchema.findById(req.params.id)
 		.then((channel: Channel | null) => res.status(200).json(channel))
@@ -58,19 +65,20 @@ channelRoute.post("/", Auth.authorize, (req: Request, res: Response) => {
 });
 
 //update a channel
-channelRoute.put("/:id", Auth.authorize, (req: Request, res: Response) => {
-	ChannelSchema.findByIdAndUpdate(req.params.id, req.body.channel)
-		.then((channel: Channel | null) => {
-			if(!channel) res.status(404).json({ msg: "Channel not found" });
-			else if(!channel.owners.includes(req.user!._id)) res.status(401).json({ msg: "You are not an owner of this channel" });
-			else res.status(200).json({ msg: "Channel updated" });
-		})
+channelRoute.put("/:id", Auth.authorize, async (req: Request, res: Response) => {
+	const channelId: string=req.params.id;
+	const channel: Channel | null = await ChannelSchema.findById(channelId);
+
+	if(!channel) return res.status(404).json({ msg: "Channel not found" });
+	else if(!channel.owners.includes(req.user!._id)) return res.status(401).json({ msg: "You are not an owner of this channel" });
+
+	ChannelSchema.findByIdAndUpdate(channelId, req.body.channel)
+		.then((_: Channel | null) => res.status(200).json({ msg: "Channel updated" }))
 		.catch((err: Error) => res.status(500).json({ msg: "Error updating channel", err: err }));
 });
 
 //modify the importance of a channel
-channelRoute.patch("/:id/importance", Auth.authorize, (req: Request, res: Response) => {
-	if(req.user?.role!="admin") return res.status(401).json({ msg: "You are not an admin" });
+channelRoute.patch("/:id/importance", Auth.authorize, Auth.isMod, (req: Request, res: Response) => {
 	ChannelSchema.findById(req.params.id)
 		.then((channel: Channel | null) => {
 			if(!channel) res.status(404).json({ msg: "Channel not found" });
