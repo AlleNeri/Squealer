b04a4ea8ce267fea71625de50006d6b0870cb684
SaadMedhat <saad2001f@gmail.com>
2024-01-26 16:41:10 +0100

    Aggiunta delle reazioni

--- social/src/components/Post/Post.jsx
+++ social/src/components/Post/Post.jsx
@@ -1,12 +1,16 @@
-import React, { useEffect, useRef } from 'react';
-import { Card, CardContent, Divider, Typography } from '@material-ui/core';
+import React, { useEffect, useRef, useContext } from 'react';
+import { Card, CardContent, Divider, Typography, IconButton } from '@material-ui/core';
 import L, {Icon} from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import markerIconPng from "leaflet/dist/images/marker-icon.png"
+import markerIconPng from "leaflet/dist/images/marker-icon.png";
+import { SentimentVeryDissatisfied, SentimentDissatisfied, SentimentSatisfied, SentimentVerySatisfied } from '@material-ui/icons';
+import { LoginContext } from '../../context/LoginContext/LoginContext';
 
 export default function Post({post}) {
     const {title, content, keywords} = post;
     const mapRef = useRef(); // Assign useRef to a variable
+    const token = localStorage.getItem('token');
+    const { loggedIn } = useContext(LoginContext);
 
     useEffect(() => {
         if(content && content.position){
@@ -31,6 +35,29 @@ export default function Post({post}) {
         }
     }, [content]);
 
+    const handleReaction = async (reaction) => {
+        try {
+          const response = await fetch(`${import.meta.env.VITE_DEFAULT_URL}/posts/${post._id}/react`, {
+            method: 'PATCH',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': token
+            },
+            body: JSON.stringify({ reaction })
+          });
+      
+          if (!response.ok) {
+            console.log(response);
+            throw new Error('Error reacting to post');
+          }
+      
+          const data = await response.json();
+          console.log(data);
+        } catch (error) {
+          console.error('Error reacting to post', error);
+        }
+      };
+
     return (
     <div style={{ display: 'flex', justifyContent: 'center' }}>
         <Card style={{ margin: '20px', backgroundColor: '#f5f5f5', borderRadius: '10px', width:'600px' }}>
@@ -46,11 +73,16 @@ export default function Post({post}) {
                     : <p></p>
                 }
                 {content && content.position && <div ref={mapRef} style={{ height: '500px', width: '100%', zIndex: 500 }}></div>}
-                {content && content.img && <img src={`http://localhost:8080/media/image/${content.img}`} alt="description" width="100%" height="500px" />}
+                {content && content.img && <img src={`${import.meta.env.VITE_DEFAULT_URL}/media/image/${content.img}`} alt="description" width="100%" height="500px" />}
                 <Divider style={{ margin: '20px 0' }} />
                 <Typography variant="body2" component="p">
                     {keywords && keywords.join(', ')}
                 </Typography>
+                <Divider style={{ margin: '20px 0' }} />
+                <IconButton onClick={() => handleReaction(-2)} disabled={!loggedIn}><SentimentVeryDissatisfied /></IconButton>
+                <IconButton onClick={() => handleReaction(-1)} disabled={!loggedIn}><SentimentDissatisfied /></IconButton>
+                <IconButton onClick={() => handleReaction(1)} disabled={!loggedIn}><SentimentSatisfied /></IconButton>
+                <IconButton onClick={() => handleReaction(2)} disabled={!loggedIn}><SentimentVerySatisfied /></IconButton>
             </CardContent>
         </Card>
     </div>
