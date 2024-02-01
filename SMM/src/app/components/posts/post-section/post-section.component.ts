dc8a32a5c9326385f55ecc99f909c893154a42a5
AlleNeri <alessandro.neri21@studio.unibo.it>
2024-02-01 23:16:54 +0100

    feat(smm): popular/unpopular post sections

--- SMM/src/app/components/posts/post-section/post-section.component.ts
+++ SMM/src/app/components/posts/post-section/post-section.component.ts
@@ -15,9 +15,13 @@ import Client from 'src/app/classes/client';
 export class PostSectionComponent implements OnInit, OnChanges {
   @Input({required: true}) client!: Client;
   protected posts: Post[];
+  protected popularPosts: Post[];
+  protected unpopularPosts: Post[];
 
   constructor(protected backendComunication: BackendComunicationService, protected authService: AuthenticationService) {
     this.posts = [];
+    this.popularPosts = [];
+    this.unpopularPosts = [];
   }
 
   ngOnInit(): void { this.loadPosts(); }
@@ -31,9 +35,13 @@ export class PostSectionComponent implements OnInit, OnChanges {
     if(this.authService.isLoggedIn())
       this.backendComunication.get(`posts/my?as=${this.client.id}`, this.authService.token!)
         //posts are sorted by date
-        .subscribe((posts: Object)=> this.posts = (posts as Post[]).sort((a: Post, b: Post)=> new Date(b.date).getTime() - new Date(a.date).getTime()));
+        .subscribe((posts: Object)=> {
+          this.posts = (posts as Post[]).sort((a: Post, b: Post)=> new Date(b.date).getTime() - new Date(a.date).getTime())
+          this.popularPosts = this.getPopularPosts();
+          this.unpopularPosts = this.getUnpopularPosts();
+        });
   }
 
-  popularPosts(): Post[] { return this.posts.filter((post: Post)=> post.popular); }
-  unpopularPosts(): Post[] { return this.posts.filter((post: Post)=> post.unpopular); }
+  getPopularPosts(): Post[] { return this.posts.filter((post: Post)=> post.popular); }
+  getUnpopularPosts(): Post[] { return this.posts.filter((post: Post)=> post.unpopular); }
 }
