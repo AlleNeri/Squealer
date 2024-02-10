ff2427029f7c9a441b8d4d991aa23250126f2bbe
SaadMedhat <saad2001f@gmail.com>
2024-02-10 12:29:39 +0100

    Squealer console log

--- social/src/App.jsx
+++ social/src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useMemo} from 'react';
+import React, {useState, useMemo, useEffect} from 'react';
 import { createBrowserRouter,  RouterProvider } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import LoginPage from './pages/Login/Login';
@@ -58,7 +58,11 @@ function App() {
 	const loginContextValue = useMemo(() => ({ loggedIn, setLoggedIn }), [loggedIn, setLoggedIn]);
 	const postsContextValue = useMemo(() => ({ posts, setPosts }), [posts, setPosts]);
 	const timeContextValue = useMemo(() => ({ updateInterval, setUpdateInterval, updateTimes, setUpdateTimes }), [updateInterval, setUpdateInterval, updateTimes, setUpdateTimes]);
-
+	
+	useEffect(() => {
+		console.log('%c SQUEALER', 'font-size: 40px; color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
+	}, []);
+	
 	return (
 		<TimeProvider value={{timeContextValue}}>
 			<SidebarProvider>
