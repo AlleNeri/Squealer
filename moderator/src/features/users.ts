253e14303db755788b58984c28407fbfa3a93247
AlleNeri <alessandro.neri21@studio.unibo.it>
2024-02-15 15:52:00 +0100

    feat(moderator): filter the users by type

--- moderator/src/features/users.ts
+++ moderator/src/features/users.ts
@@ -2,16 +2,25 @@ import { getToken } from '../utils/storage';
 import { env } from '../env';
 
 const usersId = 'users';
+const userTypes = ['normal', 'vip', 'smm', 'mod', 'bot'];
+const selectedTypes = [ ...userTypes ];
 
 export function showUsers() {
 	document.querySelector<HTMLDivElement>(`#${env.CONTENT_DIV}`)!.innerHTML = `
 		<h2>Users</h2>
+		<form class="checkbox-container" id="user-type-form">
+			${userTypes.map((userType) => 
+				`<input type="checkbox" id='${userType}' checked>
+				<label for='${userType}'>${userType}</label>`
+			).join('')}
+		</form>
 		<div id='${usersId}' class="users-container"></div>
 	`;
-	populateUsers();
+	populateUsers(selectedTypes);
+	setCheckboxListeners();
 }
 
-function populateUsers() {
+function populateUsers(type: string[]) {
 	const token: string = getToken()!;
 	fetch(
 		`${env.BACKEND_URL}/users`,
@@ -24,18 +33,19 @@ function populateUsers() {
 		.then(users => {
 			if(!users) return;
 			const usersDiv = document.querySelector<HTMLDivElement>(`#${usersId}`)!;
+			usersDiv.innerHTML = '';
 			users.map((user: any) => {
-				usersDiv.innerHTML += `
-					<div class='user'>
-						<div class='user-header'>
-							<h3>${user.u_name}</h3>
-							<i>${user.name.first} ${user.name.last}</i>
-						</div>
-						<div>
-							<button>Edit</button>
-						</div>
-					</div>
-				`;
+				if(type.includes(user.type)) usersDiv.innerHTML += `
+							<div class='user'>
+								<div class='user-header'>
+									<h3>${user.u_name}</h3>
+									<i>${user.name.first} ${user.name.last}</i>
+								</div>
+								<div>
+									<button>Edit</button>
+								</div>
+							</div>
+						`;
 			});
 		});
 }
@@ -45,5 +55,16 @@ export function addActionToUserLink(href: string): void {
 	document.querySelector<HTMLAnchorElement>(`a[href='${href}']`)!.addEventListener('click', (event) => {
 		event.preventDefault();
 		console.log('Clicked on user link', href);
+	});
+}
+
+function setCheckboxListeners() {
+	document.getElementById('user-type-form')!.addEventListener('change', (event) => {
+		const target = event.target as HTMLInputElement;
+		if(target.tagName !== 'INPUT') return;
+		if(!userTypes.includes(target.id)) return;
+		if(target.checked) selectedTypes.push(target.id);
+		else selectedTypes.splice(selectedTypes.indexOf(target.id), 1);
+		populateUsers(selectedTypes);
 	});
 }
