5dadf2b9f3963ea276d85d3cfd2a6368f76f8cb1
AlleNeri <alessandro.neri21@studio.unibo.it>
2024-02-20 17:07:01 +0100

    feat(moderator): posts type support for images and locations

--- moderator/src/features/posts.ts
+++ moderator/src/features/posts.ts
@@ -25,6 +25,10 @@ function populatePosts(): void {
 		.then(response => response.json())
 		.then(posts => {
 			if(!posts) return;
+			//order posts by date
+			posts.sort((a: any, b: any) => {
+				return new Date(b.posted_on).getTime() - new Date(a.posted_on).getTime();
+			});
 			posts.map(async (post: any) => {
 				return fetch(
 					`${env.BACKEND_URL}/users/${post.posted_by}`,
@@ -43,7 +47,21 @@ function populatePosts(): void {
 									<button>Edit</button>
 								</div>
 								<div>
-									<p>${post.content.text}</p>
+									${
+										post.content.text
+											? `<p>${post.content.text}</p>`
+											: ''
+									}
+									${
+										post.content.img
+											? `<div class="preview-image"><img src="${env.BACKEND_URL}/media/image/${post.content.img}" alt="post image not found" /></div>`
+											: ''
+									}
+									${
+										post.content.position
+											? `<p>Latitude: ${post.content.position.latitude}, Longitude: ${post.content.position.longitude}</p>`
+											: ''
+									}
 								</div>
 							</div>
 						`;
