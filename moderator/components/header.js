570dadb3096fd43636b7eb6ead708dec15674420
AlleNeri <alessandro.neri21@studio.unibo.it>
2024-02-23 18:58:47 +0100

    feat(moderator): header

---
+++ moderator/components/header.js
@@ -0,0 +1,60 @@
+class Header extends HTMLElement {
+	constructor() {
+		super();
+		this.shadow = this.attachShadow({ mode: 'open' });
+	}
+
+	get isLogged() {
+		return this.hasAttribute('isLogged')
+			? this.getAttribute('isLogged').toLowerCase() === 'true'
+			: false;
+	}
+
+	set isLogged(value) { this.setAttribute('isLogged', value); }
+
+	static get observedAttributes() { return ['isLogged']; }
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		if(name === 'isLogged') this.render();
+	}
+
+	connectedCallback() { this.render(); }
+
+	render() {
+		console.log(this.isLogged);
+		this.shadow.innerHTML = `
+			<style>
+				.header {
+					color: #ffffffa6;
+					background-color: #001529;
+					padding: 10px 50px;
+				}
+				.header ul {
+					list-style-type: none;
+					overflow: hidden;
+				}
+				.header ul li {
+					float: left;
+					padding: 0 10px;
+				}
+				.header ul li a {
+					text-decoration: none;
+					color: #ffffffa6;
+				}
+			</style>
+			<nav class="header">
+				<ul>
+					<li><a href="./index.html">Home</a></li>
+					${ this.isLogged
+						? `	<li><a href="./posts.html">Posts</a></li>
+							<li><a href="./users.html">Users</a></li>
+							<li><a href="./channels.html">Channels</a></li>`
+						: ''
+					}
+				</ul>
+			</nav>
+		`;
+	}
+}
+
+customElements.define('my-header', Header);
